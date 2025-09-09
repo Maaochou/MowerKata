57d5bdf4acd10862e271a2fe7f786fdd6c17a209
maachouO <24957623+Maaochou@users.noreply.github.com>
2025-09-09 21:32:42 +0200

    feat: add position validation

--- src/main/java/maaochou/mowerkata/domain/Position.java
+++ src/main/java/maaochou/mowerkata/domain/Position.java
@@ -1,4 +1,13 @@
 package maaochou.mowerkata.domain;
 
 public record Position(int positionOnXAxis, int positionOnYAxis) {
+
+    public Position {
+        if (positionOnXAxis < 0) {
+            throw new IllegalArgumentException("The X axis position is invalid");
+        }
+        if (positionOnYAxis < 0) {
+            throw new IllegalArgumentException("The Y axis position is invalid");
+        }
+    }
 }
